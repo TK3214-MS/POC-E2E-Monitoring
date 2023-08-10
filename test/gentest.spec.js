8f6c6aec96cb33105ef035a0a9f896a991f25aa2
Yasuki Takami <yasuki.takami@outlook.com>
2023-08-10 18:39:23 +0900

    Remove sample test file

--- test/gentest.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require("ceruleoscope");
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByRole('link', { name: 'ストア' }).click();
-  await page.locator('#hero_bday23_annouce').getByRole('link', { name: '今すぐ購入' }).click();
-  await page.getByRole('button', { name: 'Watches & Trackers' }).click();
-});
